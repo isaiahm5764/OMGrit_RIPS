9c2f70863bb1f2fe7d53c49c96df6a8ccb1a794a
Joseph Munar <jmunar@t3420a-1159ll.ipamnet.ipam.ucla.edu>
2019-07-11 15:57:43 -0700

    Fixed Validation Code

--- examples/OMGrit Example 2 Check.py
+++ examples/OMGrit Example 2 Check.py
@@ -3,20 +3,27 @@ import math
 
 tstart = 0.0
 tstop = 1.0
-n = 2048
+n = 100
 deltat = (tstop-tstart)/n
 
 sstart = 0.0
 sstop = 1.0
-m = 10
+m = 100
 deltas = (sstop-sstart)/m
 
-nu = 2
-alpha = 0.005
+nu = 0.7
+alpha = 0.05
 
-A = (nu*deltat)/(deltas**2) + deltat/(2*deltas)
-B = 1 - (2*nu*deltat)/(deltas**2)
-C = (nu*deltat)/(deltas**2) - deltat/(2*deltas)
+A = -(nu*deltat)/(deltas**2) - deltat/(2*deltas)
+B = 1 + (2*nu*deltat)/(deltas**2)
+C = deltat/(2*deltas) - (nu*deltat)/(deltas**2)
+
+Unaught = [1 for i in range(m/2)] + [0 for i in range(m/2, m+1)]
+
+#KUnaught = [Unaught[1]*B+Unaught[2]*C]
+#for i in range(2,m-1):
+#	KUnaught.append(Unaught[i-1]*A+Unaught[i]*B+Unaught[i+1]*C)
+#KUnaught.append(Unaught[m-2]*A+Unaught[m-1]*B)
 
 try:
 	# Initialize model
@@ -26,27 +33,36 @@ try:
 	Uvars = M.addVars(((i,j) for i in range(n+1) for j in range(m+1)), name='U')
 	Vvars = M.addVars(((i,j) for i in range(1,n+1) for j in range(1,m)), name='V')
 
-	# add constraints
-	M.addConstrs(Uvars[0,j] == 1 for j in range(1,m)) # initial conditions
-	M.addConstrs(Uvars[i,0] == 0 for i in range(n+1)) # boundary conditions
-	M.addConstrs(Uvars[i,m] == 0 for i in range(n+1))
+	for j in range(1,m):
+		Uvars[0,j].UB = Unaught[j] # initial conditions
+		Uvars[0,j].LB = Unaught[j]
+	for i in range(n+1):
+		Uvars[i,0].UB == 0 # boundary conditions
+		Uvars[i,0].LB == 0
+		Uvars[i,m].UB == 0
+		Uvars[i,m].LB == 0
 
-	M.addConstr(Uvars[1,1] + Uvars[1,2] - deltat*Vvars[1,1] == B+C)
-	M.addConstrs(Uvars[1,j-1] + Uvars[1,j] + Uvars[1,j+1] - deltat*Vvars[1,j] == A+B+C for j in range(2, m-1))
-	M.addConstr(Uvars[1,m-2] + Uvars[1,m-1] - deltat*Vvars[1,m-1] == A+B)
+	# add constraints
+	M.addConstr(B*Uvars[1,1] + C*Uvars[1,2] - deltat*Vvars[1,1] == Unaught[1])
+	M.addConstrs(A*Uvars[1,j-1] + B*Uvars[1,j] + C*Uvars[1,j+1] - deltat*Vvars[1,j] == Unaught[j] for j in range(2, m-1))
+	M.addConstr(A*Uvars[1,m-2] + B*Uvars[1,m-1] - deltat*Vvars[1,m-1] == Unaught[m-1])
 	for i in range(2,n+1):
-		M.addConstr(-B*Uvars[i-1,1] - C*Uvars[i-1,2] + Uvars[i,1] + Uvars[i,2] - deltat*Vvars[i,1] == 0)
-		M.addConstrs(-A*Uvars[i-1,j-1] - B*Uvars[i-1,j] - C*Uvars[i-1,j+1] + Uvars[i,j-1] + Uvars[i,j] + Uvars[i,j+1] - deltat*Vvars[i,j] == 0 for j in range(2, m-1))
-		M.addConstr(-A*Uvars[i-1,m-2] - B*Uvars[i-1,m-1] + Uvars[i,m-2] + Uvars[i,m-1] - deltat*Vvars[i,m-1] == 0)
+		M.addConstr(-Uvars[i-1,1] + B*Uvars[i,1] + C*Uvars[i,2] - deltat*Vvars[i,1] == 0)
+		M.addConstrs(-Uvars[i-1,j] + A*Uvars[i,j-1] + B*Uvars[i,j] + C*Uvars[i,j+1] - deltat*Vvars[i,j] == 0 for j in range(2, m-1))
+		M.addConstr(-Uvars[i-1,m-1] + A*Uvars[i,m-2] + B*Uvars[i,m-1] - deltat*Vvars[i,m-1] == 0)
 
 	# Set Objective
-	M.setObjective(0.5*deltat*deltas*( sum(Uvars[i,j]*Uvars[i,j] - 2*Uvars[i,j] + 1 for j in range(m+1) for i in range(n+1)) + alpha*sum(Vvars[i,j]*Vvars[i,j] for j in range(1,m) for i in range(1,n+1)) ))
+	M.setObjective(0.5*deltat*deltas*( sum((Uvars[i,j] - Unaught[j])*(Uvars[i,j] - Unaught[j]) for j in range(m+1) for i in range(n+1)) + alpha*sum(Vvars[i,j]*Vvars[i,j] for j in range(1,m) for i in range(1,n+1))))
 
 	# Prints constraints
+	#M.computeIIS()
 	M.write("Model.lp")
-
+	
 	# Solves model
+	#M.Params.Presolve = 0
 	M.optimize()
+	
+
 
 	# Writes out solution
 	docu = "" # initializes document
@@ -64,10 +80,10 @@ try:
 		else:
 			docu += str(v.x) + '\n'
 			s += 1
-			if s > m:
+			if s == m+1:
 				s = 0
 				t += 1
-			if t == 2049:
+			if t == n+1:
 				flag = True
 				t = 1
 				s = 1
